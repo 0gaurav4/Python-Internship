01a69289bc11ed2fb58e6e3a10caa6ad73c4b57a
Gaurav Tiwari <gauravtsm@gmail.com>
2023-07-28 04:05:39 +0530

    add wc

---
+++ Python programs/first/wc.py
@@ -0,0 +1,24 @@
+# author : @mysgrown
+
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+# Replace 'C:\\' with the path to your JPG image
+image_path = 'C:\\Users\\gaura\\OneDrive\\Desktop\\Python Internship\\Python programs\\first\\w2.png'
+image = Image.open(image_path)
+
+image_array = np.array(image)
+
+# Replace 'C:\\' with the path to your text file or provide your text here
+with open('C:\\Users\\gaura\\OneDrive\\Desktop\\Python Internship\\Chapters\\day1.html', 'r') as file:
+    text = file.read()
+
+wordcloud = WordCloud(background_color='black', mask=image_array, contour_color='green', contour_width=1, stopwords=set(STOPWORDS))
+wordcloud.generate(text)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
